50f7fd6fa90bb7ed8117494f6e83e460ffb78b05
09905210423 <amirdfhg.lkf103235@gmail.com>
2024-03-06 10:15:20 -0800

    P2

--- cgt.py
+++ cgt.py
@@ -1,16 +1,18 @@
 class course:
-    def __init__(self,name,teacher,namre1,namre2):
-        self.teacher=teacher
+    def __init__(self,nameteacher,namre1,namre2):
+        self.nameteacher=nameteacher
         self.namre1=namre1
         self.namre2=namre2
-        self.name=name
     def average(self):
         avg=(self.namre1+self.namre2)/2
         print(avg)
-math=course("mr.jack"12,12)
+math=course("mr.jack",20,12)
 
-science=course(13,10)
+science=course("ahmad",20,10)
 
 math.average()
 
-print(math.teacher)
+science.average()
+
+print(math.nameteacher)
+print(science.nameteacher)
